a76a5a3054833b40ae5cae1ed36a866cc0b1feda
Mateusz Gozdek <mgozdekof@gmail.com>
2019-10-31 02:19:09 +0100

    Initial commit
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

---
+++ main.tf
@@ -0,0 +1,24 @@
+# =================== ROOT CA ======================
+resource "tls_private_key" "root_ca" {
+  algorithm = "RSA"
+  rsa_bits  = var.rsa_bits
+}
+
+resource "tls_self_signed_cert" "root_ca" {
+  key_algorithm   = tls_private_key.root_ca.algorithm
+  private_key_pem = tls_private_key.root_ca.private_key_pem
+
+  subject {
+    common_name  = "root-ca"
+    organization = var.organization
+  }
+
+  is_ca_certificate     = true
+  validity_period_hours = var.validity_period_hours
+
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "cert_signing",
+  ]
+}
---
+++ variables.tf
@@ -0,0 +1,18 @@
+# Optional variables
+variable "rsa_bits" {
+  description = "Default number of RSA bits for certificates."
+  type        = string
+  default     = "4096"
+}
+
+variable "organization" {
+  description = "Organization field for certificates."
+  type        = string
+  default     = "organization"
+}
+
+variable "validity_period_hours" {
+  description = "Validity of Root CA in hours"
+  type        = string
+  default     = 8760
+}
